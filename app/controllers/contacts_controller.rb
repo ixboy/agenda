6ed0369912a7c2d8af7d975c5a01a0cc5ad8e478
ixboy <ismaelixboy@gmail.com>
2022-09-04 14:56:07 +0100

    setup new and create action for contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,25 @@
 class ContactsController < ApplicationController
   def index
+    @contacts = Contact.all
+  end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact
+      flash[:success] = 'Contato criado com sucesso.'
+      redirect_to root
+    else
+      render 'new'
+    end
+  end
+
+    private
+
+  def contact_params
+    params.require(:contact).permit(:full_name, :cpf, :email, :birth_date)
   end
 end
