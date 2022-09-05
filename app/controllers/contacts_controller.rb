84489e2a48a7da2ace19622ee447644b8da7175d
ixboy <ismaelixboy@gmail.com>
2022-09-05 02:44:18 +0100

    create user controller and create and new actions

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,6 +3,11 @@ class ContactsController < ApplicationController
     @contacts = Contact.all
   end
 
+  def show
+    @contact = Contact.find(params[:id])
+  end
+
+
   def new
     @contact = Contact.new
   end
