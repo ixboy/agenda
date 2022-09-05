25629223365f7acb736fef00e280d1ebf43594b7
ixboy <ismaelixboy@gmail.com>
2022-09-05 02:46:18 +0100

    add validations to contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,11 @@
 class Contact < ApplicationRecord
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  validates :full_name, presence: true, length: {maximum: 30}
+  validates :cpf, presence: true, length: {minimum: 11, maximum: 11 }, format: { with: /\d/}
+  validates :birth_date, presence: true
+  validates :email, presence: true, length: { maximum: 255 },
+                                    format: { with: VALID_EMAIL_REGEX },
+                                    uniqueness: { case_sensitive: true }
+
 end
