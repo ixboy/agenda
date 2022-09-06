9cbae5c09aed58a72b30d066412cb05e26bc430a
ixboy <ismaelixboy@gmail.com>
2022-09-06 20:36:42 +0100

    add validations and associations to user and contact models and

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,11 +1,22 @@
 class Contact < ApplicationRecord
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
-  validates :full_name, presence: true, length: {maximum: 30}
-  validates :cpf, presence: true, length: {minimum: 11, maximum: 11 }, format: { with: /\d/}
+  validates :full_name, presence: true, length: { maximum: 30 }
+  validates :cpf, presence: true, length: { minimum: 11, maximum: 11 }, format: { with: /\d/ }
   validates :birth_date, presence: true
   validates :email, presence: true, length: { maximum: 255 },
-                                    format: { with: VALID_EMAIL_REGEX },
-                                    uniqueness: { case_sensitive: true }
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: true }
 
+  belongs_to :user
+  has_one :address
+
+  def self.search(search)
+    if search
+      t = Contact.arel_table
+      where(t[:full_name].matches("#{search}%"))
+    else
+      all
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,21 @@
 class User < ApplicationRecord
-  has_secure_password 
+  has_secure_password
 
   before_save :email_downcase
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
-  validates :name, presence: true, length: {maximum: 15}
-  validates :password, presence: true, length: {minimum: 6}
+  validates :name, presence: true, length: { maximum: 15 }
+  validates :password, presence: true, length: { minimum: 6 }
   validates :email, presence: true, length: { maximum: 255 },
-                                    format: { with: VALID_EMAIL_REGEX },
-                                    uniqueness: { case_sensitive: true }
-
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: true }
 
   has_many :contacts
 
-
   private
-    def email_downcase
-      self.email.downcase!
-    end
+
+  def email_downcase
+    email.downcase!
+  end
 end
