9c2f163d7fcb966e46ad6e419ff7b8e5daec7821
ixboy <ismaelixboy@gmail.com>
2022-09-06 20:48:37 +0100

    update session controller and it's helpers

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+    redirect_to root_path if user_signed_in?
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      sign_in(user)
+      redirect_to root_path
+    else
+      flash.now[:danger] = 'Email e Senha inválidos'
+      render 'new'
+    end
+  end
+
+  def destroy
+    sign_out
+    flash[:warning] = 'Logout realizado com sucesso'
+    redirect_to root_path
+  end
+end
