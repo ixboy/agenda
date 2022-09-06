e77f14d074c82e32a31fc9a877df2d88ca2049e6
ixboy <ismaelixboy@gmail.com>
2022-09-06 20:43:06 +0100

    add user_id into contacts and fix all routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,20 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
-  resources :contacts
-  resources :users, only: [:new, :create]
+  get 'addresses/new'
+  get 'addresses/create'
+  get 'addresses/edit'
+  get 'addresses/destroy'
   root 'contacts#index'
+
+  get 'cadastro', to: 'users#new'
+  get 'entrar',   to: 'sessions#new'
+  post 'entrar',  to: 'sessions#create'
+  delete 'sair', to: 'sessions#destroy'
+
+  get 'entrar', to: 'sessions#new'
+
+  resources :users
+  resources :contacts do
+    resources :addresses
+  end
+  resources :sessions, only: %i[new create destroy]
 end
