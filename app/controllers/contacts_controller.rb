67aa7cb7587ba0bb2a38c56e930fd0835ae0f1c7
ixboy <ismaelixboy@gmail.com>
2022-09-06 20:31:41 +0100

    add remaining actions and fix contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,19 +1,18 @@
 class ContactsController < ApplicationController
   def index
-    @contacts = Contact.all
+    @contacts = Contact.search(params[:search])
   end
 
   def show
-    @contact = Contact.find(params[:id])
+    @contact = Contact.find_by(id: params[:id])
   end
-
 
   def new
     @contact = Contact.new
   end
 
   def create
-    @contact = Contact.new(contact_params)
+    @contact = current_user.contacts.build(contact_params)
     if @contact.save
       flash[:success] = 'Contato criado com sucesso.'
       redirect_to root_path
@@ -22,7 +21,36 @@ class ContactsController < ApplicationController
     end
   end
 
-    private
+  def edit
+    @contact = current_user.contacts.find_by(id: params[:id])
+    if @contact.nil?
+      flash[:danger] = 'Contato não encontrado.'
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @contact = current_user.contacts.find(params[:id])
+    if @contact.update(contact_params)
+      flash[:success] = 'Contato atualizado com sucesso'
+      redirect_to root_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @contact = current_user.contacts.find(params[:id])
+    if @contact.destroy
+      flash[:success] = 'Contato removido com sucesso'
+      redirect_to root_path
+    else
+      flash[:danger] = 'Contato não encontrado.'
+      redirect_to root_path
+    end
+  end
+
+  private
 
   def contact_params
     params.require(:contact).permit(:full_name, :cpf, :email, :birth_date)
