9fb0c3d086a85e8f152b8512c82f2bae7d4e4678
ixboy <ismaelixboy@gmail.com>
2022-09-06 20:30:05 +0100

    add custom callback helper and session helpers to app controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :require_logged_in_user, only: %i[edit update]
 
   def new
     @user = User.new
@@ -7,6 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = 'Cadastro realizado com sucesso'
       redirect_to root_path
     else
@@ -14,13 +16,20 @@ class UsersController < ApplicationController
     end
   end
 
-  def edit
+  def edit; end
+
+  def update
+    if current_user.update(user_params)
+      flash[:success] = 'Dados atualizados com sucesso'
+      redirect_to root_path
+    else
+      render 'edit'
+    end
   end
 
-
-
   private
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
