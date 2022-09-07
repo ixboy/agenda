712d0780a1881732ddeac4ad46a42836c0ccbc2d
ixboy <ismaelixboy@gmail.com>
2022-09-07 19:08:16 +0100

    fix validations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,7 +2,8 @@ class Contact < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
   validates :full_name, presence: true, length: { maximum: 30 }
-  validates :cpf, presence: true, length: { minimum: 11, maximum: 11 }, format: { with: /\d/ }
+  validates :cpf, presence: true, length: { minimum: 11, maximum: 11 }, format: { with: /\d/ }, 
+                    uniqueness: { case_sensitive: true }
   validates :birth_date, presence: true
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
