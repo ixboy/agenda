057d44c6fa22c8cf9b9324343f46710481325710
ixboy <ismaelixboy@gmail.com>
2022-09-10 07:07:18 +0100

    setup  services class create javascript file amd setup styles and event listeners

--- app/services/cep_api.rb
+++ app/services/cep_api.rb
@@ -2,19 +2,17 @@ require 'rest-client'
 require 'json'
 
 class CepApi
+  def initialize(cep_num)
+    @cep_num = cep_num
+    @url = "http://viacep.com.br/ws/#{cep_num}/json"
+    @headers = {
+      'content-type': 'application/json',
+      'cache-control': 'no-cache'
+    }
+  end
 
-    def initialize(cep_num="91040000")
-      @cep_num = cep_num
-      @url = "http://viacep.com.br/ws/#{cep_num}/json"
-      @headers = {
-        'content-type': "application/json",
-        'cache-control': "no-cache"
-      }
-    end    
-
-    def call
-        res = RestClient.get(@url)
-        body = JSON.parse(res, { symbolize_names: true })
-        body
-    end
+  def call
+    res = RestClient.get(@url)
+    JSON.parse(res, { symbolize_names: true })
+  end
 end
