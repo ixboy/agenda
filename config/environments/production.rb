aa4d46dce4979ca6889ff4aeeb6a99c383ae8805
ixboy <ismaelixboy@gmail.com>
2022-09-14 05:18:34 +0100

    remove unused info in production env file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,18 +99,4 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-
-  config.action_mailer.default_url_options = { host: 'agenda-info-app.herokuapp.com' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 587,
-    domain: 'gmail.com',
-    user_name: Rails.application.credentials.gmail[:user_name],
-    # password:
-    authentication: 'plain',
-    enable_starttls_auto: true,
-    open_timeout: 5,
-    read_timeout: 5
-  }
 end
